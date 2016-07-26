ceb12a57c3585283133fd1b9dd66ffbb6b83ab03
manhlh <manhlh231@gmail.com>
2016-07-27 01:46:21 +0700

    Create Project
    Create my project and key google map api

---
+++ Onthi-sms-map-json/app/src/test/java/linearlayout/example/manh/onthi_sms_map_json/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package linearlayout.example.manh.onthi_sms_map_json;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
